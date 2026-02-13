af58e1d73866d0cfa5487f03500578a422676c3d
jmhar3 <jmhar@protonmail.com>
2026-02-13 19:11:01 +1100

    handle password input submission in function

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -1,28 +1,35 @@
-import { Button, Flex, Stack, Text, TextInput } from "@mantine/core";
+import { Button, Flex, Stack, TextInput } from "@mantine/core";
+import { useNavigate } from "react-router";
 import { useState } from "react";
 import Cookies from "js-cookie";
 
 function PasswordInput() {
-  const [passwordInput, setPasswordInput] = useState("");
+  const navigate = useNavigate();
+  const [passwordInput, setPasswordInput] = useState<string | undefined>();
 
   const secretPassword = Cookies.get("secretPassword");
 
+  const correctPassword = "secret-password";
+
+  const onSubmit = () => {
+    if (passwordInput) Cookies.set("secretPassword", passwordInput);
+    if (passwordInput === correctPassword) navigate("/secret/about-me");
+  };
+
   return (
-    <Stack>
-      <Text>Input the password to enter</Text>
-      <Flex>
+    <Stack gap="sm">
+      <Flex gap="sm">
         <Stack>
           <TextInput
             value={passwordInput}
+            placeholder="INPUT PASSWORD"
+            error={secretPassword !== correctPassword && "Incorrect password"}
             onChange={(e) => {
               setPasswordInput(e.target.value);
             }}
           />
-          {secretPassword && <Text c="red">Password incorrect</Text>}
         </Stack>
-        <Button onClick={() => Cookies.set("secretPassword", passwordInput)}>
-          Submit
-        </Button>
+        <Button onClick={onSubmit}>ENTER</Button>
       </Flex>
     </Stack>
   );
