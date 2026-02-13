3c3df72e7ad41319409ea69cc3f59bf18b6d0014
jmhar3 <jmhar@protonmail.com>
2026-02-13 19:38:58 +1100

    populate menu w unstyled links

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,15 +1,45 @@
 import { useDisclosure } from "@mantine/hooks";
-import { Modal, Button } from "@mantine/core";
+import { Modal, Button, Stack, Title } from "@mantine/core";
 
 import MenuIcon from "../assets/icons/MenuIcon";
+
+interface MenuItem {
+  label: string;
+  path: string;
+  isExternal?: boolean;
+}
 
 function Menu() {
   const [opened, { open, close }] = useDisclosure(false);
 
+  const menu: MenuItem[] = [
+    { label: "Who am I?", path: "about-me" },
+    { label: "Working with me", path: "working-with-me" },
+    { label: "Explore My Work", path: "portfolio" },
+    { label: "Packages", path: "/packages" },
+    { label: "Frequently Asked Questions", path: "faq" },
+    { label: "Get in touch", path: "/contact" },
+    { label: "Studio Space", path: "www.studioxixi.com.au", isExternal: true },
+  ];
+
   return (
     <>
       <Modal fullScreen opened={opened} onClose={close} withCloseButton={false}>
-        Modal without header, press escape or click on overlay to close
+        <Stack align="center">
+          <Title size="6em">Melbourne Art Natural</Title>
+
+          {menu.map(({ label, path, isExternal }) => (
+            <Button
+              variant="transparent"
+              key="label"
+              component="a"
+              href={path}
+              target={isExternal ? "_blank" : undefined}
+            >
+              {label.toUpperCase()}
+            </Button>
+          ))}
+        </Stack>
       </Modal>
 
       <Button
