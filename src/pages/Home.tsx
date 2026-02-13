46a17b400ec20b65334a8902665ccbb6d0f00f77
jmhar3 <jmhar@protonmail.com>
2026-02-13 17:00:48 +1100

    populate unstyled home screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,39 @@
-import { Text } from "@mantine/core";
+import { Box, Button, Stack, Text, Title } from "@mantine/core";
+import { useLocation, useNavigate } from "react-router";
+import Cookies from "js-cookie";
+
+import RightArrowIcon from "../assets/icons/RightArrowIcon";
+import PasswordInput from "../components/PasswordInput";
 
 function Home() {
-  return <Text>Hello World</Text>;
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const isSecret = pathname.includes("secret");
+
+  const correctPassword = "secret-password";
+  const secretPassword = isSecret && Cookies.get("secretPassword");
+
+  return (
+    <Box>
+      <Stack>
+        <Title>Melbourne Art Natural</Title>
+        <Text>PROFESSIONAL PORTRAIT PHOTOGRAPHER</Text>
+        {isSecret && secretPassword !== correctPassword ? (
+          <PasswordInput />
+        ) : (
+          <Button
+            justify="center"
+            rightSection={<RightArrowIcon />}
+            onClick={() =>
+              navigate(isSecret ? "/secret/about-me" : "/about-me")
+            }
+          >
+            Enter
+          </Button>
+        )}
+      </Stack>
+    </Box>
+  );
 }
 
 export default Home;
