efc253683b3a756c339d8e68e5f4c0ec8d5c9366
jmhar3 <jmhar@protonmail.com>
2026-02-13 19:39:27 +1100

    add basic contact form according to contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,10 +1,45 @@
-import { Text } from "@mantine/core";
+import { Accordion, Button, Stack, Text, TextInput } from "@mantine/core";
+
 import PageLayout from "./PageLayout";
 
+import contactImage from "../assets/images/m.a.n.png";
+import { useState } from "react";
+
 function Contact() {
+  const [value, setValue] = useState<string | null>("panel-1");
+
   return (
-    <PageLayout>
-      <Text>Get in touch</Text>
+    <PageLayout label="Get in touch" image={contactImage}>
+      <Stack w="100%">
+        <Accordion w="100%" value={value} onChange={setValue}>
+          <Accordion.Item value="panel-1">
+            <Text>Contact Info</Text>
+
+            <Accordion.Panel>
+              <TextInput />
+              <Button onClick={() => setValue("panel-2")}>Next</Button>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="panel-2">
+            <Text>Inquiry Details</Text>
+
+            <Accordion.Panel>
+              <TextInput />
+              <Button onClick={() => setValue("panel-1")}>Prev</Button>
+              <Button onClick={() => setValue("panel-3")}>Next</Button>
+            </Accordion.Panel>
+          </Accordion.Item>
+          <Accordion.Item value="panel-3">
+            <Text>Inspiration</Text>
+
+            <Accordion.Panel>
+              <TextInput />
+              <Button onClick={() => setValue("panel-2")}>Prev</Button>
+              <Button onClick={() => setValue("panel-3")}>Next</Button>
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </Stack>
     </PageLayout>
   );
 }
