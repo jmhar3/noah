2f7bb5a7f0efca89956e915bc869546d68d02fd6
jmhar3 <jmhar@protonmail.com>
2026-02-14 16:04:44 +1100

    create portfolio helper data

---
+++ src/helpers/portfolio.ts
@@ -0,0 +1,145 @@
+export interface GalleryType {
+  pathname: string;
+  label: string;
+  description?: string;
+  images: string[];
+  isSecret?: boolean;
+}
+
+export const portfolio: GalleryType[] = [
+  {
+    pathname: "/digital",
+    label: "Digital",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/film",
+    label: "Film",
+    description:
+      "This is a description about film. This is a description about film. This is a description about film. This is a description about film. This is a description about film.",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/group",
+    label: "Group / Couples",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/inside",
+    label: "Inside",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/outdoors",
+    label: "Outdoors",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/masc",
+    label: "Masc",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/product",
+    label: "Product / Brand",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/secret",
+    label: "Erotic",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+    isSecret: true,
+  },
+  {
+    pathname: "/over18",
+    label: "Shibari / Kink / 18+",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/themed",
+    label: "Themed / Holiday",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/travel",
+    label: "Travel / Street",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+  {
+    pathname: "/video",
+    label: "Video",
+    images: [
+      "https://www.adobe.com/la/creativecloud/file-types/image/raster/media_13f659e708c031c519b546ba716f2cdc0d34c90ec.jpeg?width=1200&format=pjpg&optimize=medium",
+      "https://kenchris.github.io/wasm-webp/test3.webp",
+      "https://www.widerangegalleries.com/images/xl/20191028-Giau-Glow.jpg",
+      "https://i.sstatic.net/3xDTD.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3D0gIztuV_7sysh9m6dHnt5gmbyTwBPqxJQ&s",
+    ],
+  },
+];
