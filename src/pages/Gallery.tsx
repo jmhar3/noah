0cf3e13725be7ff96bbc5c43734382aea0b58461
jmhar3 <jmhar@protonmail.com>
2026-02-14 16:05:24 +1100

    roughly build gallery page

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,12 +1,53 @@
-import { Text } from "@mantine/core";
+import { useLocation, Navigate } from "react-router";
+import { Image, Text } from "@mantine/core";
+
 import PageLayout from "./PageLayout";
 
+import { portfolio, type GalleryType } from "../helpers/portfolio";
+import { useState } from "react";
+
 function Gallery() {
-  return (
-    <PageLayout>
-      <Text>Gallery</Text>
-    </PageLayout>
+  const { pathname } = useLocation();
+  const isSecret = pathname.includes("secret");
+
+  const gallery = portfolio.find((galleryItem) => {
+    return pathname.includes(galleryItem.pathname);
+  });
+
+  return gallery ? (
+    <View gallery={gallery} />
+  ) : (
+    <Navigate to={isSecret ? "/secret/portfolio" : "/portfolio"} />
   );
 }
 
 export default Gallery;
+
+function View({ gallery }: { gallery: GalleryType }) {
+  const { label, description, images } = gallery;
+
+  const [selectedImage, setSelectedImage] = useState(images[0]);
+  const [displayedImages, setDisplayedImages] = useState(
+    gallery.images.slice(1),
+  );
+
+  const onSelectImage = (selectedImage: string) => {
+    setSelectedImage(selectedImage);
+    setDisplayedImages(images.filter((image) => image !== selectedImage));
+  };
+
+  return (
+    <PageLayout label={label} image={selectedImage}>
+      <Text size="lg">{description}</Text>
+      {displayedImages.map((image) => (
+        <Image
+          key={image}
+          src={image}
+          loading="lazy"
+          decoding="async"
+          onClick={() => onSelectImage(image)}
+        />
+      ))}
+    </PageLayout>
+  );
+}
