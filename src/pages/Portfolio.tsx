cf22a48d9b0166259522095271426d5562eab5e5
jmhar3 <jmhar@protonmail.com>
2026-02-16 23:32:31 +1100

    add gallery select functionality to portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,53 +1,77 @@
-import { BackgroundImage, Button, Flex, Title } from "@mantine/core";
-import PageLayout from "./PageLayout";
-import { portfolio } from "../helpers/portfolio";
-import { Carousel } from "@mantine/carousel";
+import { Divider, Stack, Text, Title } from "@mantine/core";
+import { useLocation, useNavigate } from "react-router";
 import { useState } from "react";
 
+import PageLayout from "./PageLayout";
+import GalleryModal from "../components/GalleryModal";
+
+import { portfolio } from "../helpers/portfolio";
+import type { GalleryType } from "../helpers/portfolio";
+
 function Portfolio() {
-  const [focusedSlide, setFocusedSlide] = useState(0);
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+  const isSecret = pathname.includes("secret");
+
+  const [focusedGallery, setFocusedGallery] = useState<
+    GalleryType | undefined
+  >();
+  const [showGalleryModal, setShowGalleryModal] = useState(
+    pathname.includes("portfolio/"),
+  );
+
+  const [selectedGallery, setSelectedGallery] = useState(
+    portfolio.find((galleryItem) => {
+      return pathname.includes(galleryItem.pathname);
+    }),
+  );
+
+  const onSelectGallery = (gallery: GalleryType) => {
+    setSelectedGallery(gallery);
+    setShowGalleryModal(true);
+    navigate(`${isSecret ? "/secret" : ""}/portfolio${gallery.pathname}`);
+  };
+
+  const onCloseGallery = () => {
+    setShowGalleryModal(false);
+    navigate(`${isSecret ? "/secret" : ""}/portfolio`);
+  };
 
   return (
-    <PageLayout>
-      <Title pt="1.5em" size="4.5em">
-        Explore My Work
-      </Title>
+    <PageLayout label="Explore My Work" image={focusedGallery?.images[0]}>
+      <Stack>
+        {portfolio.map((portfolioGallery, index) => {
+          return (
+            <>
+              {index !== 0 && <Divider />}
+              <Stack
+                key={portfolioGallery.pathname}
+                style={{ cursor: "pointer" }}
+                onMouseEnter={() => setFocusedGallery(portfolioGallery)}
+                onClick={() => onSelectGallery(portfolioGallery)}
+              >
+                <Title>{portfolioGallery.label}</Title>
+                {portfolioGallery && focusedGallery === portfolioGallery && (
+                  <Text
+                    h={focusedGallery === portfolioGallery ? "auto" : "0px"}
+                    style={{ transition: "ease-out height 3s" }}
+                  >
+                    {portfolioGallery.description}
+                  </Text>
+                )}
+              </Stack>
+            </>
+          );
+        })}
+      </Stack>
 
-      <Carousel
-        pos="fixed"
-        left="-6em"
-        bottom="6em"
-        slideGap="lg"
-        slideSize="28%"
-        withIndicators
-        withControls={false}
-        onSlideChange={(slideIndex) => setFocusedSlide(slideIndex)}
-        emblaOptions={{
-          loop: true,
-          startIndex: 0,
-          align: "start",
-        }}
-      >
-        {portfolio.map((gallery, index) => (
-          <Carousel.Slide key={gallery.pathname}>
-            <Flex align="flex-end" justify="flex-end" h="80vh">
-              <BackgroundImage radius="sm" src={gallery.images[0]}>
-                <Button
-                  component="a"
-                  variant="transparent"
-                  href={`/portfolio${gallery.pathname}`}
-                  h={focusedSlide + 2 === index ? "80vh" : "50vh"}
-                  style={{ transition: "height 0.3s ease-in-out" }}
-                >
-                  <Flex h="100%" align="flex-end" justify="flex-end">
-                    <Title size="3em">{gallery.label}</Title>
-                  </Flex>
-                </Button>
-              </BackgroundImage>
-            </Flex>
-          </Carousel.Slide>
-        ))}
-      </Carousel>
+      {selectedGallery && (
+        <GalleryModal
+          isOpen={showGalleryModal}
+          onClose={onCloseGallery}
+          gallery={selectedGallery}
+        />
+      )}
     </PageLayout>
   );
 }
