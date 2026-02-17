26e845da2e4c331bb4e0ef1eaeda787f1b8dc777
jmhar3 <jmhar@protonmail.com>
2026-02-17 16:27:25 +1100

    set default portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,4 +1,4 @@
-import { Divider, Stack, Text, Title } from "@mantine/core";
+import { Divider, Stack, Text } from "@mantine/core";
 import { useLocation, useNavigate } from "react-router";
 import { useState } from "react";
 
@@ -13,9 +13,9 @@ function Portfolio() {
   const { pathname } = useLocation();
   const isSecret = pathname.includes("secret");
 
-  const [focusedGallery, setFocusedGallery] = useState<
-    GalleryType | undefined
-  >();
+  const [focusedGallery, setFocusedGallery] = useState<GalleryType | undefined>(
+    portfolio[0],
+  );
   const [showGalleryModal, setShowGalleryModal] = useState(
     pathname.includes("portfolio/"),
   );
@@ -45,20 +45,46 @@ function Portfolio() {
             <>
               {index !== 0 && <Divider />}
               <Stack
+                pt="3px"
                 key={portfolioGallery.pathname}
-                style={{ cursor: "pointer" }}
                 onMouseEnter={() => setFocusedGallery(portfolioGallery)}
                 onClick={() => onSelectGallery(portfolioGallery)}
+                style={{
+                  cursor: "pointer",
+                  gap:
+                    portfolioGallery === focusedGallery &&
+                    portfolioGallery.description
+                      ? undefined
+                      : 0,
+                  transition:
+                    portfolioGallery === focusedGallery
+                      ? "gap 0.15s ease-in"
+                      : "gap 0.15s ease-out",
+                }}
               >
-                <Title>{portfolioGallery.label}</Title>
-                {portfolioGallery && focusedGallery === portfolioGallery && (
-                  <Text
-                    h={focusedGallery === portfolioGallery ? "auto" : "0px"}
-                    style={{ transition: "ease-out height 3s" }}
-                  >
-                    {portfolioGallery.description}
-                  </Text>
-                )}
+                <Text
+                  size="1.4em"
+                  c={
+                    portfolioGallery === focusedGallery
+                      ? "firebrick"
+                      : undefined
+                  }
+                >
+                  {portfolioGallery.label.toUpperCase()}
+                </Text>
+                <Text
+                  style={{
+                    maxHeight:
+                      portfolioGallery === focusedGallery ? "500px" : 0,
+                    transition:
+                      portfolioGallery === focusedGallery
+                        ? "max-height 0.35s ease-in"
+                        : "max-height 0.15s ease-out",
+                    overflow: "hidden",
+                  }}
+                >
+                  {portfolioGallery.description}
+                </Text>
               </Stack>
             </>
           );
