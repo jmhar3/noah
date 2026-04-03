a5c423e6455d802275c429586b971bd8527412c5
jmhar3 <jmhar@protonmail.com>
2026-04-03 18:26:09 +1100

    handle mailing list submission

--- src/pages/ManKind.tsx
+++ src/pages/ManKind.tsx
@@ -1,4 +1,4 @@
-import { useDisclosure, useOrientation } from "@mantine/hooks";
+import { useDisclosure, useOrientation, useTimeout } from "@mantine/hooks";
 import Countdown from "react-countdown";
 
 import {
@@ -13,6 +13,7 @@ import {
 
 import image from "../assets/images/m.a.n.png";
 import { useState } from "react";
+import { supabase } from "../supabase";
 
 const landscapeImages = [image];
 const portraitImages = [image];
@@ -41,14 +42,57 @@ function ManKind() {
   const { type } = useOrientation();
 
   const [email, setEmail] = useState<string>();
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const [submissionSuccess, setSubmissionSuccess] = useState<boolean | null>(
+    null,
+  );
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+  const [successMessage, setSuccessMessage] = useState<string | undefined>(
+    "Successfully signed up to mailing list.",
+  );
+  const { start: showSuccessMessage } = useTimeout(
+    () => setSuccessMessage(undefined),
+    6000,
+  );
 
   const onCloseJoinMailingList = () => {
     setEmail(undefined);
     closeMailingListModal();
   };
 
-  const onJoinMailingList = () => {
-    onCloseJoinMailingList();
+  const onJoinMailingList = async () => {
+    setIsSubmitting(true);
+    setErrorMessage(null);
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (email && emailRegex.test(email)) {
+      try {
+        const { status, error } = await supabase
+          .from("mailing_list")
+          .insert({ email: email });
+
+        if (status === 201) {
+          showSuccessMessage();
+          setSubmissionSuccess(true);
+          onCloseJoinMailingList();
+          setErrorMessage(null);
+        }
+
+        if (status === 400) {
+          console.error(error);
+          setSubmissionSuccess(false);
+          setErrorMessage(error?.message || "An error occured");
+        }
+      } catch (error) {
+        console.error(error);
+        setSubmissionSuccess(false);
+        setErrorMessage("An error occured");
+      } finally {
+        setIsSubmitting(false);
+      }
+    } else {
+      setErrorMessage("Invalid Email");
+    }
   };
 
   return (
@@ -75,28 +119,44 @@ function ManKind() {
             <Countdown date={countdownDate} />
           </Text>
 
-          {showMailingListModal ? (
-            <Flex gap="sm">
-              <TextInput
-                px="md"
-                bdrs="md"
-                size="md"
-                value={email}
-                variant="unstyled"
-                bd="1px solid floralwhite"
-                placeholder="YOUR EMAIL"
-                onChange={(event) => setEmail(event.target.value)}
-              />
-              <Button
-                size="md"
-                radius="md"
-                variant="outline"
-                color="floralwhite"
-                onClick={onJoinMailingList}
-              >
-                SIGN UP
-              </Button>
-            </Flex>
+          {submissionSuccess ? (
+            successMessage && <Text pl="13px">{successMessage}</Text>
+          ) : showMailingListModal ? (
+            <Stack gap="3" align="center">
+              <Flex gap="sm">
+                <TextInput
+                  bdrs="md"
+                  size="md"
+                  type="email"
+                  value={email}
+                  variant="unstyled"
+                  placeholder="YOUR EMAIL"
+                  bd="1px solid floralwhite"
+                  onChange={(event) => setEmail(event.target.value)}
+                  styles={{
+                    input: {
+                      paddingLeft: "12px",
+                      paddingRight: "12px",
+                      color: "floralwhite",
+                      backgroundColor: "none",
+                    },
+                  }}
+                />
+                <Button
+                  size="md"
+                  radius="md"
+                  variant="outline"
+                  color="floralwhite"
+                  onClick={onJoinMailingList}
+                  loading={isSubmitting}
+                >
+                  SIGN UP
+                </Button>
+              </Flex>
+              <Text pl="13px" c="red">
+                {errorMessage}
+              </Text>
+            </Stack>
           ) : (
             <Button
               size="md"
