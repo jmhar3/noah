c205b76087f6e8bc26622d06abe16c54cb191a53
jmhar3 <jmhar@protonmail.com>
2026-06-11 15:31:47 +1000

    hide cursor text on home menu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,30 +41,35 @@ function Home() {
     <BackgroundImage src={lakeLandscape}>
       <MenuModal isOpen={opened} />
 
-      <Box
-        pos="fixed"
-        id="cursor-text"
-        style={{
-          zIndex: "9999",
-          pointerEvents: "none",
-          transform: "translate(-50%, -50%)",
-        }}
-        className="circular-text"
-      >
-        {letters.map((letter, index) => {
-          const rotationDeg = (360 / letters.length) * index;
-          const factor = Math.PI / letters.length;
-          const x = factor * index;
-          const y = factor * index;
-          const transform = `rotateZ(${rotationDeg}deg) translate3d(${x}px, ${y}px, 0)`;
+      {!opened && (
+        <Box
+          pos="fixed"
+          id="cursor-text"
+          style={{
+            zIndex: "9999",
+            pointerEvents: "none",
+            transform: "translate(-50%, -50%)",
+          }}
+          className="circular-text"
+        >
+          {letters.map((letter, index) => {
+            const rotationDeg = (360 / letters.length) * index;
+            const factor = Math.PI / letters.length;
+            const x = factor * index;
+            const y = factor * index;
+            const transform = `rotateZ(${rotationDeg}deg) translate3d(${x}px, ${y}px, 0)`;
 
-          return (
-            <span key={index} style={{ transform, WebkitTransform: transform }}>
-              {letter}
-            </span>
-          );
-        })}
-      </Box>
+            return (
+              <span
+                key={index}
+                style={{ transform, WebkitTransform: transform }}
+              >
+                {letter}
+              </span>
+            );
+          })}
+        </Box>
+      )}
 
       <Center py="8.5em" h="100vh" w="100vw" c="floralwhite" onClick={open}>
         <Stack h="100%" align="center" justify="space-between">
