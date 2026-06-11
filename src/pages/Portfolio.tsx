56dae74802c5b0464f6bf48ed6b2bc1f5d2d1572
jmhar3 <jmhar@protonmail.com>
2026-06-11 14:16:08 +1000

    redesign portfolio functionality

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,67 +1,68 @@
-import { Divider, Stack, Text } from "@mantine/core";
-import { useLocation, useNavigate } from "react-router";
+import { Divider, Flex, Image, ScrollArea, Stack, Text } from "@mantine/core";
+import { useLocation } from "react-router";
 import { useState } from "react";
 
 import PageLayout from "./PageLayout";
 import GalleryModal from "../components/GalleryModal";
 
 import { portfolio } from "../helpers/portfolio";
+
+import defaultImage from "../assets/images/beach_portrait.webp";
+
 import type { GalleryType } from "../helpers/portfolio";
 
 function Portfolio() {
-  const navigate = useNavigate();
   const { pathname } = useLocation();
   const isSecret = pathname.includes("secret");
+  console.log(isSecret);
 
-  const [focusedGallery, setFocusedGallery] = useState<GalleryType | undefined>(
-    portfolio[0],
-  );
-  const [showGalleryModal, setShowGalleryModal] = useState(
-    pathname.includes("portfolio/"),
-  );
+  const [previewImage, setPreviewImage] = useState<string>(defaultImage);
+  const [focusedGallery, setFocusedGallery] = useState<
+    GalleryType | undefined
+  >();
+  const [showGalleryModal, setShowGalleryModal] = useState(false);
+  const [focusedImage, setFocusedImage] = useState<string | undefined>();
 
-  const [selectedGallery, setSelectedGallery] = useState(
-    portfolio.find((galleryItem) => {
-      return pathname.includes(galleryItem.pathname);
-    }),
-  );
-
-  const onSelectGallery = (gallery: GalleryType) => {
-    setSelectedGallery(gallery);
+  const onFocusImage = (image: string) => {
     setShowGalleryModal(true);
-    navigate(`${isSecret ? "/secret" : ""}/portfolio${gallery.pathname}`);
+    setFocusedImage(image);
   };
 
   const onCloseGallery = () => {
     setShowGalleryModal(false);
-    navigate(`${isSecret ? "/secret" : ""}/portfolio`);
+    setFocusedImage(undefined);
   };
 
   return (
-    <PageLayout label="Explore My Work" image={focusedGallery?.images[0]}>
-      <Stack>
+    <PageLayout label="Explore My Work" image={previewImage}>
+      <Stack gap="0">
         {portfolio.map((portfolioGallery, index) => {
           return (
-            <>
-              {index !== 0 && <Divider />}
-              <Stack
-                pt="3px"
-                key={portfolioGallery.pathname}
-                onMouseEnter={() => setFocusedGallery(portfolioGallery)}
-                onClick={() => onSelectGallery(portfolioGallery)}
-                style={{
-                  cursor: "pointer",
-                  gap:
-                    portfolioGallery === focusedGallery &&
-                    portfolioGallery.description
-                      ? undefined
-                      : 0,
-                  transition:
-                    portfolioGallery === focusedGallery
-                      ? "gap 0.15s ease-in"
-                      : "gap 0.15s ease-out",
-                }}
-              >
+            <Stack
+              pb="sm"
+              gap="xs"
+              key={portfolioGallery.pathname}
+              onMouseEnter={() => setPreviewImage(portfolioGallery.images[0])}
+              onMouseLeave={() =>
+                setPreviewImage(focusedGallery?.images[0] || defaultImage)
+              }
+              onClick={() => setFocusedGallery(portfolioGallery)}
+              style={{
+                cursor: "pointer",
+                gap:
+                  portfolioGallery === focusedGallery &&
+                  portfolioGallery.description
+                    ? undefined
+                    : 0,
+                transition:
+                  portfolioGallery === focusedGallery
+                    ? "gap 0.15s ease-in"
+                    : "gap 0.15s ease-out",
+              }}
+            >
+              <Stack gap="sm">
+                {index !== 0 && <Divider color="#b44655" />}
+
                 <Text
                   size="1.4em"
                   c={
@@ -72,30 +73,45 @@ function Portfolio() {
                 >
                   {portfolioGallery.label.toUpperCase()}
                 </Text>
-                <Text
-                  style={{
-                    maxHeight:
-                      portfolioGallery === focusedGallery ? "500px" : 0,
-                    transition:
-                      portfolioGallery === focusedGallery
-                        ? "max-height 0.35s ease-in"
-                        : "max-height 0.15s ease-out",
-                    overflow: "hidden",
-                  }}
-                >
-                  {portfolioGallery.description}
-                </Text>
               </Stack>
-            </>
+
+              <Stack
+                style={{
+                  maxHeight: portfolioGallery === focusedGallery ? "500px" : 0,
+                  transition:
+                    portfolioGallery === focusedGallery
+                      ? "max-height 0.5s ease-in"
+                      : "max-height 0.35s ease-out",
+                  overflow: "hidden",
+                }}
+              >
+                <Text size="xl">{portfolioGallery.description}</Text>
+
+                <ScrollArea type="scroll" offsetScrollbars>
+                  <Flex gap="sm">
+                    {portfolioGallery.images.map((image) => (
+                      <Image
+                        mah="9em"
+                        key={image}
+                        src={image}
+                        bdrs="sm"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => onFocusImage(image)}
+                      />
+                    ))}
+                  </Flex>
+                </ScrollArea>
+              </Stack>
+            </Stack>
           );
         })}
       </Stack>
 
-      {selectedGallery && (
+      {focusedImage && (
         <GalleryModal
           isOpen={showGalleryModal}
           onClose={onCloseGallery}
-          gallery={selectedGallery}
+          image={focusedImage}
         />
       )}
     </PageLayout>
