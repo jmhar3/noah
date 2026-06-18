e4adf79d6b7910f0280789eef4b4c756f7d0c1a3
jmhar3 <jmhar@protonmail.com>
2026-06-18 13:51:30 +1000

    redesign portfolio

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,83 +1,79 @@
 import { useState } from "react";
-import { Text, Flex, Stack, Image, Accordion, ScrollArea } from "@mantine/core";
+import { Tabs, Text, Stack, Accordion, SegmentedControl } from "@mantine/core";
 
 import PageLayout from "./PageLayout";
-import GalleryModal from "../components/GalleryModal";
+import PortfolioImage from "../components/PortfolioImage";
 
 import { portfolio } from "../helpers/portfolio";
 
 function Portfolio() {
-  const [showGalleryModal, setShowGalleryModal] = useState(false);
-  const [focusedImage, setFocusedImage] = useState<string | undefined>();
-  const [accordionState, setAccordionState] = useState<string>("0");
-
-  const onFocusImage = (image: string) => {
-    setShowGalleryModal(true);
-    setFocusedImage(image);
-  };
-
-  const onCloseGallery = () => {
-    setShowGalleryModal(false);
-    setFocusedImage(undefined);
-  };
+  const [activeTab, setActiveTab] = useState<string | null>("0");
 
   return (
     <PageLayout
       label="Explore My Work"
-      image={portfolio[Number(accordionState)].images[0]}
+      image={portfolio[Number(activeTab)].images[0]}
     >
-      <Accordion
-        value={accordionState}
-        onChange={(index) => setAccordionState(index || "0")}
-        styles={{ item: { borderColor: "#b44655" } }}
+      <Tabs
+        variant="unstyled"
+        color="#b44655"
+        value={activeTab}
+        onChange={setActiveTab}
       >
+        <SegmentedControl
+          fullWidth
+          radius="sm"
+          bg="floralwhite"
+          color="#b44655"
+          bd="solid 1px steelblue"
+          value={activeTab || "0"}
+          onChange={(value: string) => setActiveTab(value)}
+          data={[
+            { label: "DIGITAL", value: "0" },
+            { label: "FILM", value: "1" },
+            { label: "KINK", value: "2" },
+          ]}
+        />
+        {/*<Tabs.List grow>
+          {portfolio.map((portfolioGallery, index) => {
+            return (
+              <Tabs.Tab key={portfolioGallery.label} value={index.toString()}>
+                <Text
+                  py="md"
+                  size="1.4em"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => setActiveTab(index.toString())}
+                >
+                  {portfolioGallery.label.toUpperCase()}
+                </Text>
+              </Tabs.Tab>
+            );
+          })}
+        </Tabs.List>*/}
+
         {portfolio.map((portfolioGallery, index) => {
           return (
-            <Accordion.Item
+            <Tabs.Panel
               w="100%"
               value={index.toString()}
               key={portfolioGallery.pathname}
             >
-              <Text
-                py="md"
-                size="1.4em"
-                style={{ cursor: "pointer" }}
-                onClick={() => setAccordionState(index.toString())}
-              >
-                {portfolioGallery.label.toUpperCase()}
-              </Text>
-              <Accordion.Panel>
-                <Stack>
-                  <Text size="xl">{portfolioGallery.description}</Text>
+              <Stack pt="lg">
+                <Text size="xl">{portfolioGallery.description}</Text>
 
-                  <ScrollArea type="scroll" offsetScrollbars>
-                    <Flex gap="sm">
-                      {portfolioGallery.images.map((image) => (
-                        <Image
-                          mah="9em"
-                          key={image}
-                          src={image}
-                          bdrs="sm"
-                          style={{ cursor: "pointer" }}
-                          onClick={() => onFocusImage(image)}
-                        />
-                      ))}
-                    </Flex>
-                  </ScrollArea>
-                </Stack>
-              </Accordion.Panel>
-            </Accordion.Item>
+                {portfolioGallery.images.map((image) => (
+                  <PortfolioImage key={image} image={image} />
+                ))}
+              </Stack>
+            </Tabs.Panel>
           );
         })}
-      </Accordion>
-
-      {focusedImage && (
-        <GalleryModal
-          isOpen={showGalleryModal}
-          onClose={onCloseGallery}
-          image={focusedImage}
-        />
-      )}
+      </Tabs>
+      <Accordion
+        value={activeTab}
+        onChange={(index) => setActiveTab(index || "0")}
+        styles={{ item: { borderColor: "#b44655" } }}
+      ></Accordion>
     </PageLayout>
   );
 }
