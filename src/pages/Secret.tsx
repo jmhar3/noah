f3495c95ab1a58e58786a33faa4afeb6665f8ba5
jmhar3 <jmhar@protonmail.com>
2026-06-18 15:54:12 +1000

    create Secret page

---
+++ src/pages/Secret.tsx
@@ -0,0 +1,14 @@
+import { Stack, Text } from "@mantine/core";
+
+function Secret() {
+  return (
+    <Stack>
+      <Text>About</Text>
+      <Text>Packages</Text>
+      <Text>Examples</Text>
+      <Text>Testimonials</Text>
+    </Stack>
+  );
+}
+
+export default Secret;
